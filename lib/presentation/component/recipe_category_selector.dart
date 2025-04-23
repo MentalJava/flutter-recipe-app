edc4dce7b9123776c3ca41d15c2de9c9fde47238
Si Yujun <siuj0810@gmail.com>
2025-04-23 22:38:48 +0900

    design: dish card, recipe category selector component create

---
+++ lib/presentation/component/recipe_category_selector.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class RecipeCategorySelector extends StatelessWidget {
+  const RecipeCategorySelector({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
